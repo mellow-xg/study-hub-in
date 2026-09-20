ab844cea1b74a1f18fd1cf41d321cae44a219892
mellow-xg <ibrahimhere0709@gmail.com>
2026-09-20 17:48:11 +0530

    Handle Android back button inside Study Hub WebView

--- android/app/src/main/java/com/studyhub/app/MainActivity.java
+++ android/app/src/main/java/com/studyhub/app/MainActivity.java
@@ -1,5 +1,20 @@
 package com.studyhub.app;
 
+import android.webkit.WebView;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+    @Override
+    public void onBackPressed() {
+        WebView webView = getBridge() != null ? getBridge().getWebView() : null;
+
+        if (webView != null && webView.canGoBack()) {
+            webView.goBack();
+            return;
+        }
+
+        super.onBackPressed();
+    }
+}
